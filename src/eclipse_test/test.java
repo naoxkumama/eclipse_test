43a3390bf8528fb1bd36941f08e01c99681a68e8
Takahashi <ten.years.new.decade@gmail.com>
2025-04-21 13:27:02 +0900

    入力

--- src/eclipse_test/test.java
+++ src/eclipse_test/test.java
@@ -1,6 +1,12 @@
 package eclipse_test;
 
-public class test 
-{
-  変更しました。
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class test  {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // ここで入力を受け取り、合計を表示するコードを記述してください。
+    }
 }
