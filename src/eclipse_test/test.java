4eca1c64eb418b41b9dff6b33344a00911f59514
Takahashi <ten.years.new.decade@gmail.com>
2025-04-21 14:17:10 +0900

    future01修正したよ         }

--- src/eclipse_test/test.java
+++ src/eclipse_test/test.java
@@ -9,5 +9,6 @@ public class test  {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // ここで入力を受け取り、合計を表示するコードを記述してください。
     //1回目
+        //future01修正したよ
         }
 }
